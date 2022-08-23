d963b83e07cb6197219478f82266400f0aed6802
NicolaBozzi97 <108057933+NicolaBozzi97@users.noreply.github.com>
2022-08-23 17:23:34 +0200

    Last Experiments

--- Back-End/Index.js
+++ Back-End/Index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 //utlizziamo metodo express per creare istanza di express
 const app = express()
-//esperimento Json con file importato
+//esperimento Json con file importato. Poi bisognerà aggiungere un DB vero e proprio
 const {persone} = require('./persone')
 //utilizziamo file nella cartella public
 app.use(express.static('public'))
@@ -22,9 +22,22 @@ app.get('/contatti', (req, res) => {
   })
 
 //esperimento con json
-app.get('/nomi', (req, res) => {
-    res.json(persone)
+app.get('/persone', (req, res) => {
+  //esperimento mapping per mandare campi specifici di un oggetto Json. Idea Eneriko su presentazione dati consumi
+    const nuovePersone = persone.map((persona)=>{
+      const {nome, cognome, eta} = persona
+      return {nome,cognome,eta}
+
+    })
+    res.json(nuovePersone)
 })
+
+//esperimento per richiedere campi specifici su un oggetto Json. Idea Eneriko su presentazione dati consumi
+app.get('/persone/:id',(req,res)=>{
+  const {id} = req.params
+  const persona = persone.find((persona)=> persona.id === id)
+    res.json(persona)
+  })
 
 //creaiamo errore per pagine che non esistono
 app.all('*', (req, res) => {
