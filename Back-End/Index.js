48daef2b2a77a33cb8c4a2da1f5300690a524770
NicolaBozzi97 <108057933+NicolaBozzi97@users.noreply.github.com>
2022-08-23 14:39:31 +0200

    inizializzazione del server

---
+++ Back-End/Index.js
@@ -0,0 +1,29 @@
+//importiamo express
+const express = require('express')
+//utlizziamo metodo express per creare istanza di express
+const app = express()
+
+//utilizziamo file nella cartella public
+app.use(express.static('public'))
+
+//inizializziamo richieste e risposte del server
+//homepage
+app.get('/', (req, res) => {
+    res.sendFile('homepage.html', {root: __dirname + "/public"})
+  })
+//pagine about
+app.get('/about', (req, res) => {
+    res.sendFile('about.html', {root: __dirname + "/public"})
+  })
+//pagine contatti
+app.get('/contatti', (req, res) => {
+    res.sendFile('contatti.html', {root: __dirname + "/public"})
+  })
+
+//creaiamo errore per pagine che non esistono
+app.all('*', (req, res) => {
+    res.sendFile('error.html', {root: __dirname + "/public"})
+})
+
+//server ascolta alla porta 3000
+app.listen(3000)
