9c56cc24f1ddac1d1723fabbf624d440a956ff4a
NicolaBozzi97 <108057933+NicolaBozzi97@users.noreply.github.com>
2022-08-25 11:02:30 +0200

    Prototipo alternativo

---
+++ data-service/consumi.js
@@ -0,0 +1,31 @@
+const consumi = [
+    {
+        id: "1",
+        nome: "Gas",
+        costo: "1,092216 € per m3",
+        consumoMensile: "120 m3",
+        consumoGiornaliero:"4 m3",
+        costoMensile: "131,04 €",
+        costoGiornaliero: "4,36 €"
+    },
+    {
+        id: "2",
+        nome: "Acqua",
+        costo: "1,3725 per m3 €",
+        consumoMensile: "30 m3",
+        consumoGiornaliero:"1 m3",
+        costoMensile: "41,175 €",
+        costoGiornaliero: "1,3735 €"
+    },
+    {
+        id: "3",
+        nome: "Luce",
+        costo: "0,23 €/KWh",
+        consumoMensile: "225 KWh",
+        consumoGiornaliero:"7,5 KWh",
+        costoMensile: "51,75 €",
+        costoGiornaliero: "1,725 €"
+    }
+]
+
+module.exports = {consumi}
---
+++ data-service/index.js
@@ -0,0 +1,41 @@
+//importiamo express
+const express = require('express')
+
+//utlizziamo metodo express per creare istanza di express
+const app = express()
+
+//importo modulo consumi
+const {consumi} = require('./consumi')
+
+//assegnare porta 3001 a una variabile
+const PORT  = 3001
+
+//homapege
+app.get('/', (req,res) => {
+  res.send("<h1>Homepage</h1><a href='/consumi'>Vai a consumi </a>")
+})
+
+//Pagina con informazioni ridotte sui consumi
+app.get('/consumi', (req,res)=>{ 
+  const consumoRidotto = consumi.map((consumo)=>{
+    const {nome, consumoMensile } = consumo
+    return{nome, consumoMensile}
+  })
+  res.json(consumoRidotto)
+})
+
+//selezionare tutti i dati di un solo consumo utilizzando route params
+app.get('/consumi/:idConsumi', (req,res)=>{
+  const{idConsumi} = req.params
+  const consumo = consumi.find((consumo)=> consumo.id === idConsumi)
+//gestione pagine non esistenti con errore 404
+  if(!consumo){
+    return res.status(404).send("Consumo inesistente")
+  }
+  res.json(consumo)
+})
+
+
+
+//server ascolta alla porta 3001
+app.listen(PORT)
