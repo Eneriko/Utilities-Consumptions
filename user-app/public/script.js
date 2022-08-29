18e59c0f676288d6282453e11dd9a0981f7d4952
cadondo33 <cadoni.andrea.99@gmail.com>
2022-08-29 18:17:39 +0200

    variable name added to be used in homepage.html

--- user-app/public/script.js
+++ user-app/public/script.js
@@ -1,30 +1,39 @@
 let usernamee 
 let passwordd 
 
-let users = [
+let users = [  //'name' will be the name displayed in homepage.html 
     {
         username: "cadoniandrea",
         password: "bacistellari33",
+        name: "Andrea",
     },
     {
         username: "bozzinicola",
         password: "Pluto11",
+        name: "Nicola",
     },
     {
         username: "caushllarieneriko",
         password: "paperino10.",
+        name: "Eneriko",
     },
     {
         username: "gigio33",
         password: "password1",
+        name: "Gigio",
+    },
+    {
+        username: "kate99",
+        password: "mycat",
+        name: "Kate",
     }
 ]
 
 handleLogin = () => {
-    let usernamee = document.getElementById('fname')
-    let passwordd = document.getElementById('lname')
+    let usernamee = document.getElementById('fname').value
+    let passwordd = document.getElementById('lname').value
     for(let i = 0; i < users.length; i++){
-        if((users[i].username) && (users[i].password)) {
+        if((users[i].username === usernamee) && (users[i].password === passwordd)) {
             alert("FUNZIONA!!!")
             let url = location.href
             //comando per richiamare la pagina "homepage.html"
