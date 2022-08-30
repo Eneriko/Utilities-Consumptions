423c8c7a50bc738dbaa4e91e6815c658b47235c7
cadondo33 <cadoni.andrea.99@gmail.com>
2022-08-30 10:17:44 +0200

    homepage main functions created with relative JS

--- user-app/public/script.js
+++ user-app/public/script.js
@@ -1,5 +1,6 @@
 let usernamee 
 let passwordd 
+var tempName
 
 let users = [  //'name' will be the name displayed in homepage.html 
     {
@@ -35,9 +36,19 @@ handleLogin = () => {
     for(let i = 0; i < users.length; i++){
         if((users[i].username === usernamee) && (users[i].password === passwordd)) {
             alert("FUNZIONA!!!")
-            let url = location.href
+            tempName = users[i].name
+            
+            //let url = location.href
             //comando per richiamare la pagina "homepage.html"
         }
     }
     alert("Username or password not found.")
+}
+
+homePageElem = () => {
+    var today = new Date()
+    var day = today.getDay()
+    var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"]
+    document.getElementById('user').innerHTML = `Welcome back ${tempName}!`
+    document.getElementById('todayMSG').innerHTML = `Today is ${daylist[day]} and you've already spent ...`
 }
